5a87fd63f269ac19be07e3c3ec8953e665076567
Yassir <elkhailiyassir@gmail.com>
2023-10-30 11:11:12 +0100

    fixed font size hero title

--- middleware/script.ts
+++ middleware/script.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { createTransport } from "nodemailer";
 import fetch from "node-fetch";
 
-const verifyCaptcha = async (requestData: any) => {
+const verifyCaptcha = async (requestData: any): Promise<boolean> => {
   try {
     const response = await fetch(
       `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SERVER_KEY}&response=${requestData["g-recaptcha-response"]}`,
